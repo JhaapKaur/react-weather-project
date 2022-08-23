4e1bf943afbc8fa153fffb8ef56c276f9bdd6e55
JhaapKaur <jhaap@hotmail.com>
2022-08-23 21:39:24 +0100

    Added Bootstrap
    added bootsrap

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Weather Search </h1>
-        <Weather />
+      <header className="App">
+        <h1> Weather App </h1>
+        <footer>
+          This project was coded by Jhaap Kaur and is{" "}
+          <a
+            href="https://github.com/JhaapKaur/react-weather-project"
+            target="_blank"
+          >
+            open-sourced on GitHUb
+          </a>
+        </footer>
       </header>
     </div>
   );
