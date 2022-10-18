c8a1bcdd256ab57691603fe3c92721ea4d2f5a85
JhaapKaur <jhaap@hotmail.com>
2022-10-18 21:13:30 +0100

    Spacing added

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,10 +17,10 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <div className="float-left">
+            <div className="d-flex">
               <WeatherIcon code={props.data.icon} size={52} />
 
-              <div className="Float-left">
+              <div className="float-left">
                 <WeatherTemperature celsius={props.data.temperature} />
               </div>
             </div>
